22cb0054e43de4f9acc9e971a3628dc4973b294f
kaike <manokaike3@gmail.com>
2024-01-08 18:30:29 -0300

    Dessafio Dio Classificador Hero

---
+++ Desafio do Hero.py
@@ -0,0 +1,24 @@
+nome=input("Digite o nome do herói: ")
+xp=int(input("Digite o seu XP: "))
+
+if xp <=1000:
+    print(f"O Herói {nome} está no nivel Bronze");
+elif xp <=5000:
+    print(f"O heroi {nome} está no nivel Prata");
+elif xp <=7000: 
+    print(f"O heroi {nome} está no nivel Ouro");
+elif xp <=8000:
+    print(f"O heroi {nome} está no nivel Platina");
+elif xp <=9000: 
+    print(f"O heroi {nome} está no nivel Ascendente");
+elif xp <=10000:
+    print(f"O heroi {nome} está no nivel Imortal");
+else:
+    print(f"O Herói {nome} está no nivel Radiante");
+    
+
+        
+    
+    
+
+
